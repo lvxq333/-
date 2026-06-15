84406b355075a49d233932bf17ad718900830417
haibolv <lxq30380@gmail.com>
2026-06-15 22:28:10 +0800

    配置阿里云数据仓库

---
+++ src/main/java/com/hmdp/properties/AliOssProperties.java
@@ -0,0 +1,21 @@
+package com.hmdp.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * AliOssProperties：阿里云文件上传属性类
+ * 读取application-dev.yml中的配置，Spring启动后会自动加载
+ */
+@Component
+@ConfigurationProperties(prefix = "spring.alioss")
+@Data
+public class AliOssProperties {
+
+    private String endpoint;
+    private String accessKeyId;
+    private String accessKeySecret;
+    private String bucketName;
+
+}
