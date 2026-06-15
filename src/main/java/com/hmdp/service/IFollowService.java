b5cbbee53e09de290ed266129ae3bc297f0b2fff
haibolv <lxq30380@gmail.com>
2026-06-15 23:36:28 +0800

    关注或者取消关注

--- src/main/java/com/hmdp/service/IFollowService.java
+++ src/main/java/com/hmdp/service/IFollowService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,18 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IFollowService extends IService<Follow> {
 
+    /**
+     * 关注或者取消关注
+     * @param followUserId
+     * @param isFollow
+     * @return
+     */
+    Result follow(Long followUserId, Boolean isFollow);
+
+    /**
+     * 查询是否关注
+     * @param followUserId
+     * @return
+     */
+    Result isFollow(Long followUserId);
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,14 +1,20 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +23,55 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 关注或者取消关注
+     *
+     * @param followUserId: 被关注用户id
+     * @param isFollow:     是否关注
+     * @return
+     */
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        // 1.获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        String key = "follow:" + userId;
+        // 2.判断是关注还是取消关注
+        if (isFollow) {
+            // 2.1 关注,新增数据
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            boolean isSuccess = save(follow);
+        } else {
+            // 2.2 取消关注，删除数据
+            // delete from tb_follow where user_id = ? and follow_user_id = ?
+            remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId)
+                    .eq("follow_user_id", followUserId)
+            );
+        }
+        return Result.ok();
+    }
+
+    /**
+     * 查询是否关注
+     *
+     * @param followUserId
+     * @return
+     */
+    @Override
+    public Result isFollow(Long followUserId) {
+        // 1.获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        String key = "follow:" + userId;
+        // 2.查询是否关注
+        // select count(*) from tb_follow where user_id = ? and follow_user_id = ?
+        Integer count = query().eq("user_id", userId)
+                .eq("follow_user_id", followUserId).count();
+        // 3.返回结果
+        return Result.ok(count > 0);
+    }
 }
