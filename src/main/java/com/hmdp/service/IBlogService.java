58735384c43825aa1812b85ccf46b9036153ae4d
haibolv <lxq30380@gmail.com>
2026-06-15 22:31:13 +0800

    查询blog详情与点赞情况

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,24 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    /**
+     * 查询博客详情
+     * @param id
+     * @return
+     */
+    Result queryBlogById(Long id);
+
+    /**
+     * 查询热门博客
+     * @param current
+     * @return
+     */
+    Result queryHotBlog(Integer current);
+
+    /**
+     * 点赞博客
+     * @param id
+     * @return
+     */
+    Result likeBlog(Long id);
 }
