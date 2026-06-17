20535bbc8424ca78f8128092bb4c4001872beb87
haibolv <lxq30380@gmail.com>
2026-06-17 22:00:09 +0800

    将店铺的地理信息写入redis

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -8,12 +8,19 @@ import com.hmdp.utils.RedisIdWork;
 import io.lettuce.core.Value;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -24,6 +31,9 @@ class HmDianPingApplicationTests {
     private CacheClient cacheClient;
     @Resource
     private RedisIdWork redisIdWorker;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     // 线程池
     private ExecutorService es = Executors.newFixedThreadPool(500);
 
@@ -64,6 +74,36 @@ class HmDianPingApplicationTests {
             Shop shop = shopService.getById((long)i);
             cacheClient.setWithLogicExpire(
                     RedisConstants.CACHE_SHOP_KEY + (long)i, shop, 10L, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * 将店铺的地理信息写入redis
+     */
+    @Test
+    void loadShopData() {
+        // 1.查询店铺信息
+        List<Shop> list = shopService.list();
+        // 2.把店铺分组，按照typeId分组，typeId一致的放到一个集合
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 3.分批完成写入Redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            // 3.1.获取类型id
+            Long typeId = entry.getKey();
+            String key = RedisConstants.SHOP_GEO_KEY + typeId;
+            // 3.2.获取同类型的店铺的集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            // 3.3.写入redis GEOADD key 经度 纬度 member
+            for (Shop shop : value) {
+                // stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+            // 3.4.写入redis:以zset 方式存储，其中分数为店铺的经纬度，member为店铺id
+            stringRedisTemplate.opsForGeo().add(key, locations);
         }
     }
 
